a0bdc31f76e3f91772c186fdbde901d19a4b4c2a
Volodymyr Popov <appforcelab@gmail.com>
2023-11-11 13:48:39 +0000

    done task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+let colorChangeInterval;
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+startButton.addEventListener('click', startColorChange);
+stopButton.addEventListener('click', stopColorChange);
+
+function startColorChange() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  colorChangeInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorChange() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  clearInterval(colorChangeInterval);
+}
