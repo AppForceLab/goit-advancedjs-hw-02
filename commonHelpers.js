a2f3ae63946eadf4a07bab6b64ac52c07f222978
AppForceLab <107247047+AppForceLab@users.noreply.github.com>
2023-11-11 13:48:55 +0000

    Deploying to gh-pages from @ AppForceLab/goit-advancedjs-hw-02@a0bdc31f76e3f91772c186fdbde901d19a4b4c2a 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */function a(){return`#${Math.floor(Math.random()*16777215).toString(16).padStart(6,"0")}`}let o;const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",r);e.addEventListener("click",n);function r(){t.disabled=!0,e.disabled=!1,o=setInterval(()=>{document.body.style.backgroundColor=a()},1e3)}function n(){t.disabled=!1,e.disabled=!0,clearInterval(o)}
+//# sourceMappingURL=commonHelpers.js.map
